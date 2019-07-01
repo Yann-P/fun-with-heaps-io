5e0a77b9197c6a4504990a4c6fe2224904793bff
Yann Pellegrini <mail@yann-p.fr>
2019-07-02 00:21:49 +0200

    Implement rotation of the rubiks cube

--- 2-rubiks-cube/Main.hx
+++ 2-rubiks-cube/Main.hx
@@ -1,12 +1,9 @@
-import hxsl.Types.Sampler2D;
+import h3d.Vector;
+import h3d.Matrix;
 import h3d.scene.Object;
 import hxd.IndexBuffer;
 import h3d.prim.Polygon;
 import h3d.col.Point;
-import h3d.prim.Quads;
-import h3d.scene.DirLight;
-import h3d.mat.Texture;
-import h3d.mat.Material;
 import h3d.scene.Mesh;
 import h3d.prim.Cube;
 
@@ -62,20 +59,59 @@ class ShadedSubCube extends Object {
 		mesh.material.mainPass.addShader(new SubCubeShader());
 	}
 }
+
+class SubCube {
+	private var _obj:Object = null;
+	private var _baseTransform:Matrix = new Matrix();
+	private var _turnTransform:Matrix = new Matrix();
+	private var _pos:Vector = new Vector(0, 0, 0);
+
+	public function new(parent:Object, pos:Vector) {
+		_obj = new Object(parent);
+		_pos = pos;
+
+		_turnTransform.identity();
+
+		_baseTransform.identity();		
+		_baseTransform.scale(.95, .95, .95);
+		_baseTransform.translate(pos.x, pos.y, pos.z);
+
+		var a = new SubCubeFace(_obj, 0xffff00);
+		var b = new SubCubeFace(_obj, 0xff0000);
 		b.rotate(Math.PI / 2, 0, 0);
-		var c = new SubCubeFace(this, 0x0099ff);
+		var c = new SubCubeFace(_obj, 0x0099ff);
 		c.rotate(0, 0, Math.PI / 2);
-		var d = new SubCubeFace(this, 0x00ff00);
+		var d = new SubCubeFace(_obj, 0x00ff00);
 		d.rotate(0, 0, -Math.PI / 2);
-		var e = new SubCubeFace(this, 0xff9900);
+		var e = new SubCubeFace(_obj, 0xff9900);
 		e.rotate(-Math.PI / 2, 0, 0);
-		var f = new SubCubeFace(this, 0xffffff);
-		f.rotate(Math.PI, 0,  0);
+		var f = new SubCubeFace(_obj, 0xffffff);
+		f.rotate(Math.PI, 0, 0);
+
+		_obj.setTransform(_baseTransform);
+		
+	}
+
+	public function move(axis: UInt, direction: Bool, back: Bool) {
+		var pos = [_pos.x, _pos.y, _pos.z][axis];
+		var rotationAmount = Math.PI / 2 * (direction ? 1 : -1);
+		var axis = new Vector((axis == 0 ? 1 : 0), (axis == 1 ? 1 : 0), (axis == 2 ? 1 : 0));
+		
+		if (pos == 0 && !back) {
+			_turnTransform.rotateAxis(axis, rotationAmount);
+		}
+		if (pos == 2 && back) {
+			_turnTransform.rotateAxis(axis, rotationAmount);
+		}
+
+		var result = new Matrix();
+		result.multiply(_turnTransform, _baseTransform);
+		_obj.setTransform(result);
 	}
 }
 
 class SubCubeFace extends Object {
-	public function new(parent: Object, color: UInt) {
+	public function new(parent:Object, color:UInt) {
 		super(parent);
 
 		var buffer = new IndexBuffer();
@@ -88,55 +124,60 @@ class SubCubeFace extends Object {
 		// idxClockwise.reverse();
 		// idxClockwise.map(i -> buffer.push(i));
 
-		var prim = new Polygon([
-			new Point(0, 0, 0), 
-			new Point(1, 0, 0), 
-			new Point(1, 0, 1),
-			new Point(0, 0, 1)], buffer);
+		var prim = new Polygon([new Point(0, 0, 0), new Point(1, 0, 0), new Point(1, 0, 1), new Point(0, 0, 1)], buffer);
 		prim.addNormals();
 		prim.addTangents();
 		prim.translate(-.5, -.5, -.5);
 
-		trace(prim.triCount());
-
-
 		var mesh = new Mesh(prim, this);
 		mesh.material.color.setColor(color);
 		mesh.material.shadows = false;
-
 	}
 }
 
-class Main extends hxd.App {
+class RubiksCube {
+	private var _cubes:Array<SubCube> = [];
+	private var N = 3;
 
-	var t: Float = 0;
-	var b: SubCubeFace = null;
-
-	override function init() {
-
-		for(x in 0...3) {
-			for(y in 0...3) {
-				for(z in 0...3) {
-					var c = new SubCube(s3d);
-					c.scale(0.9);
-					c.x = x;
-					c.y = y;
-					c.z = z;
+	public function new(parent: Object) {
+		for (x in 0...N) {
+			for (y in 0...N) {
+				for (z in 0...N) {
+					var s = new SubCube(parent, new Vector(x, y, z));
+					_cubes.push(s);
 				}
 			}
 		}
-		
+	}
+
+	public function rotate(axis: UInt, direction: Bool, back: Bool) {
+		for(c in _cubes) {
+			c.move(axis, direction, back);
+		}
+	}
+
+}
+
+class Main extends hxd.App {
+	var t:Float = 0;
+	var _cube: RubiksCube = null;
+	override function init() {
+		_cube = new RubiksCube(s3d);
 		s3d.lightSystem.ambientLight.set(1, 1, 1);
 
 		new h3d.scene.CameraController(s3d).loadFromCamera();
 	}
 
-	override function update(dt: Float) {
-		t += dt;
+	override function update(dt:Float) {
+		t += 1;//dt;
+		
+		// move stuff around
+		if(t % 100 == 0) {
+			_cube.rotate(cast(t / 100, UInt) % 3, false, false);
+		}
 	}
 
 	static function main() {
 		new Main();
 	}
-
 }
